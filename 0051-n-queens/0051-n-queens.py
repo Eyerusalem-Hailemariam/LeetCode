ad74cd96483dbd8f774af53673025e017b83fda4
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-03 08:44:57 +0300

    Time: 8 ms (81.04%), Space: 19.7 MB (69.34%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,45 +1,40 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+    def solveNQueens(self, n: int) -> list[list[str]]:
         res = []
-
-        cols = set()       # columns that already have queens
-        pos_diag = set()   # r + c diagonals
-        neg_diag = set()   # r - c diagonals
-
-        queens = [-1] * n  # queens[row] = column where queen is placed
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+        
+        # 1D array to track queen positions: queens[row] = col
+        queens = [-1] * n
 
         def backtrack(r):
-            # All rows have queens placed
             if r == n:
+                # We only build the board strings here at the very end
                 board = []
-
-                for row in range(n):
-                    line = ["."] * n
-                    line[queens[row]] = "Q"
-                    board.append("".join(line))
-
+                for c in queens:
+                    row_str = "." * c + "Q" + "." * (n - 1 - c)
+                    board.append(row_str)
                 res.append(board)
                 return
 
             for c in range(n):
-                # Check if this position is attacked
                 if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
                     continue
 
-                # Place queen
-                queens[r] = c
+                # Place the queen in our 1D array
                 cols.add(c)
                 pos_diag.add(r + c)
                 neg_diag.add(r - c)
+                queens[r] = c
 
                 backtrack(r + 1)
 
-                # Remove queen (backtrack)
-                queens[r] = -1
+                # Backtrack
                 cols.remove(c)
                 pos_diag.remove(r + c)
                 neg_diag.remove(r - c)
+                # No need to reset queens[r] because it just gets overwritten later
 
         backtrack(0)
-
         return res
