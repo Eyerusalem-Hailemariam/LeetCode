32d8a822d76ab2ec46c5a3fff4b27502b7c23ae8
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-03 08:34:25 +0300

    Time: 11 ms (65.9%), Space: 20 MB (9.56%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,32 +1,45 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res = []
-        cols = set()
-        pos_diag = set()
-        neg_diag = set()
-        board = [["."] * n for _ in range(n)]
+
+        cols = set()       # columns that already have queens
+        pos_diag = set()   # r + c diagonals
+        neg_diag = set()   # r - c diagonals
+
+        queens = [-1] * n  # queens[row] = column where queen is placed
 
         def backtrack(r):
+            # All rows have queens placed
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+                board = []
+
+                for row in range(n):
+                    line = ["."] * n
+                    line[queens[row]] = "Q"
+                    board.append("".join(line))
+
+                res.append(board)
                 return
 
             for c in range(n):
+                # Check if this position is attacked
                 if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
                     continue
 
+                # Place queen
+                queens[r] = c
                 cols.add(c)
                 pos_diag.add(r + c)
                 neg_diag.add(r - c)
-                board[r][c] = "Q"
 
                 backtrack(r + 1)
 
+                # Remove queen (backtrack)
+                queens[r] = -1
                 cols.remove(c)
                 pos_diag.remove(r + c)
                 neg_diag.remove(r - c)
-                board[r][c] = "."
 
         backtrack(0)
+
         return res
