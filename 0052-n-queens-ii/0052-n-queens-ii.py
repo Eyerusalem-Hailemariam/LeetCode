720b440b22e372a24522e633ad4987a6b01610ee
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-03 09:37:54 +0300

    Time: 3 ms (96.14%), Space: 19.4 MB (52.75%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         res = 0
-        cols = set()
-        pos_diag = set()
-        neg_diag = set()
-
-        def backtrack(r):
+        
+        def backtrack(r, cols, diags1, diags2):
             nonlocal res
             if r == n:
                 res += 1
                 return
-
-            for c in range(n):
-                if c in cols or (r + c) in pos_diag or (r - c) in neg_diag:
-                    continue
-
-                cols.add(c)
-                pos_diag.add(r + c)
-                neg_diag.add(r - c)
-
-                backtrack(r + 1)
-
-                cols.remove(c)
-                pos_diag.remove(r + c)
-                neg_diag.remove(r - c)
-
-        backtrack(0)
+            
+            available_positions = ((1 << n) - 1) & ~(cols | diags1 | diags2)
+            
+            while available_positions:
+                position = available_positions & -available_positions
+                
+                available_positions -= position
+               
+                backtrack(
+                    r + 1, 
+                    cols | position, 
+                    (diags1 | position) << 1, 
+                    (diags2 | position) >> 1
+                )
+                
+        backtrack(0, 0, 0, 0)
         return res
