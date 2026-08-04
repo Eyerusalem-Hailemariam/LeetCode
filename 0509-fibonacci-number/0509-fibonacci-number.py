ad5c3197acf1f511caf0d2432ebe98359ea5798e
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-04 17:43:39 +0300

    Time: 286 ms (32.38%), Space: 19.3 MB (18.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,13 +1,8 @@
 class Solution:
-
-    def __init__(self):
-        self.memo = {}
         
     def fib(self, n: int) -> int:
         if n <= 1:
             return n
-        if n in self.memo:
-            return self.memo[n]
         
-        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
-        return self.memo[n]
+        return self.fib(n - 1) + self.fib(n - 2)
+        
