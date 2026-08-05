d94663347acbaca509d7ce5edcddfb224d7598f2
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-05 12:30:40 +0300

    Time: 0 ms (100%), Space: 19.5 MB (9.27%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,25 +2,19 @@ class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
         if not digits:
             return []
-        
-        phone_map = {
-            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
-        }
-        
-        result = []
-        
-        def backtrack(index: int, current_path: list):
-            if len(current_path) == len(digits):
-                result.append("".join(current_path))
-                return
-           
-            possible_letters = phone_map[digits[index]]
-            
-            for letter in possible_letters:
-                current_path.append(letter)
-                backtrack(index + 1, current_path)
-                current_path.pop()
 
-        backtrack(0, [])
-        return result
+        phone_map = {
+            "2": "abc", "3" : "def", "4":"ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9" : "wxyz"
+        }
+
+        dp = [""]
+
+        for digit in digits:
+            next_combination = []
+            for combination in dp:
+                for letter in phone_map[digit]:
+                    next_combination.append(combination + letter)
+        
+            dp = next_combination
+        return  dp
