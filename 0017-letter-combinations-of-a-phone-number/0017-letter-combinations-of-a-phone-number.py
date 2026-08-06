73837e6c5f9c08630b2b1396db05072883638fc1
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-06 11:08:59 +0300

    Time: 0 ms (100%), Space: 19.3 MB (40.24%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,19 +2,21 @@ class Solution:
     def letterCombinations(self, digits: str) -> list[str]:
         if not digits:
             return []
-
+        res = []
         phone_map = {
-            "2": "abc", "3" : "def", "4":"ghi", "5": "jkl",
-            "6": "mno", "7": "pqrs", "8": "tuv", "9" : "wxyz"
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
-
-        dp = [""]
-
-        for digit in digits:
-            next_combination = []
-            for combination in dp:
-                for letter in phone_map[digit]:
-                    next_combination.append(combination + letter)
         
-            dp = next_combination
-        return  dp
+        def backtrack(index, curr):
+            if len(digits) == len(curr):
+                res.append("".join(curr))
+                return
+
+            for letter in phone_map[digits[index]]:
+                curr.append(letter)
+                backtrack(index + 1, curr)
+                curr.pop()
+            
+        backtrack(0, [])
+        return res
