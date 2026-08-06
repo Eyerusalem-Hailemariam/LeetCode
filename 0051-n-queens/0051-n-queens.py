b31ab32b7bec50616e3b5a479995f57ed8b04fe1
Eyerusalem-Hailemariam <140660877+Eyerusalem-Hailemariam@users.noreply.github.com>
2026-08-06 15:20:48 +0300

    Time: 5 ms (98.05%), Space: 19.7 MB (69.54%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,38 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> list[list[str]]:
-        res = []
-        cols = [False] * n
+        res =[]
+        col = [False] * n
         pos_diag = [False] * (2 * n - 1)
         neg_diag = [False] * (2 * n - 1)
-        
-        queens = [-1] * n
+
+        queen = [-1] * n
 
         def backtrack(r):
             if r == n:
-               
                 board = []
-                for c in queens:
-                    row_str = "." * c + "Q" + "." * (n - 1 - c)
-                    board.append(row_str)
+                for c in queen:
+                    row_string = "." * c + "Q" + "." * (n - 1 - c)
+                    board.append(row_string)
                 res.append(board)
-                return
 
+            
             for c in range(n):
-                if cols[c] or pos_diag[r + c] or neg_diag[r - c + n - 1]:
+                if col[c] or pos_diag[r + c] or neg_diag[r - c + n -1]:
                     continue
-
-              
-                cols[c] = True
+                
+                col[c] = True
                 pos_diag[r + c] = True
                 neg_diag[r - c + n - 1] = True
-                queens[r] = c
+                queen[r] = c
 
                 backtrack(r + 1)
 
-                
-                cols[c] = False
+                col[c] = False
                 pos_diag[r + c] = False
-                neg_diag[r - c + n - 1] = False
+                neg_diag[r - c + n -1 ] = False
 
         backtrack(0)
         return res
